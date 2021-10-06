7577317914f62b992b0d5ef3f655c7664d062d03
David <dop4398@g.rit.edu>
2021-10-06 00:20:30 -0400

    box moves

--- Multiplayer Bullet Hell/Assets/Scripts/PlayerController.cs
+++ Multiplayer Bullet Hell/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    #region fields
+    public float walkingSpeed = 6.0f;
+    public float gravity = 20.0f;
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    #endregion
+
+    
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        Movement();
     }
+
+    #region helper methods
+    /// <summary>
+    /// Moves the player through inputs.
+    /// </summary>
+    public void Movement()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        float curSpeedX = walkingSpeed * Input.GetAxis("Vertical");
+        float curSpeedY = walkingSpeed * Input.GetAxis("Horizontal");
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        // Apply gravity
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+    #endregion
 }
