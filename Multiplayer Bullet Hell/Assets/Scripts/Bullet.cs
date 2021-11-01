156fdca30202115552709320069e038dd73f312f
David <dop4398@g.rit.edu>
2021-11-01 01:24:16 -0400

    lotsa good work

--- Multiplayer Bullet Hell/Assets/Scripts/Bullet.cs
+++ Multiplayer Bullet Hell/Assets/Scripts/Bullet.cs
@@ -4,16 +4,47 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    Vector3 move = new Vector3(1.0f, 0.0f, 0.0f);
+    public Vector3 direction;
+    public int speed;
+    private Camera cam;
 
     void Start()
     {
-        
+        cam = Camera.main;
+        speed = 7;
+        SetBulletDirection();
+        this.transform.position += direction;
     }
 
     void Update()
     {
-        this.transform.position += move * Time.deltaTime;
+        this.transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void SetBulletDirection()
+    {
+        Vector3 point = new Vector3();
+        Vector2 mousePos = new Vector2();
+
+        mousePos.x = Input.mousePosition.x;
+        mousePos.y = Input.mousePosition.y;
+        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+
+        direction = Vector3.Normalize(point - transform.position);
+        direction.y = 0;
+        direction.Normalize();
+    }
+
+    public void SetBulletSpeed(int s)
+    {
+        speed = s;
+    }
+
+    public void ResetBullet(Vector3 pos)
+    {
+        this.transform.position = pos;
+        SetBulletDirection();
+        this.transform.position += direction;
     }
 
     private void OnTriggerEnter(Collider other)
--- Multiplayer Bullet Hell/Assets/Scripts/PlayerController.cs
+++ Multiplayer Bullet Hell/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,28 @@ using UnityEngine.Networking;
 public class PlayerController : NetworkBehaviour
 {
     #region fields
-    public float walkingSpeed = 6.0f;
+    public float walkingSpeed = 4.0f;
     public float gravity = 20.0f;
-    CharacterController characterController;
-    Vector3 moveDirection = Vector3.zero;
+    private CharacterController characterController;
+    private Vector3 moveDirection = Vector3.zero;
 
+    private Camera cam;
+
+    private List<GameObject> bulletPool = new List<GameObject>();
     public GameObject bullet;
+    private int currentBulletIndex = 0;
     #endregion
 
-    
+
     void Start()
     {
+        for(int i = 0; i < 10; i++)
+        {
+            bulletPool.Add(Instantiate(bullet));
+            //bulletPool[i].SetActive(false);
+        }
         characterController = GetComponent<CharacterController>();
+        cam = Camera.main;
     }
 
     
@@ -25,8 +35,12 @@ public class PlayerController : NetworkBehaviour
     {
         if(isLocalPlayer)
         {
-            Movement();
-            Shoot();
+            CmdMove();
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                CmdShoot();
+            }
         }       
     }
 
@@ -34,7 +48,8 @@ public class PlayerController : NetworkBehaviour
     /// <summary>
     /// Moves the player through inputs.
     /// </summary>
-    private void Movement()
+    [Command]
+    private void CmdMove()
     {
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
@@ -53,15 +68,41 @@ public class PlayerController : NetworkBehaviour
         characterController.Move(moveDirection * Time.deltaTime);
     }
 
-    private void Shoot()
+    [Command]
+    private void CmdShoot()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        //bullet.transform.position = this.transform.position;
+        //GameObject bulletClone = Instantiate(bullet);
+        //NetworkServer.Spawn(bulletClone);
+
+        //bulletPool[currentBulletIndex].SetActive(true);
+        bulletPool[currentBulletIndex].GetComponent<Bullet>().ResetBullet(this.transform.position);
+        NetworkServer.Spawn(bulletPool[currentBulletIndex]);
+
+        currentBulletIndex++;
+        if(currentBulletIndex >= bulletPool.Count)
         {
-            Debug.Log("Shoot");
-            bullet.transform.position = this.transform.position;
-            GameObject bulletClone = Instantiate(bullet);
-            NetworkServer.Spawn(bulletClone);
+            currentBulletIndex = 0;
         }
+    }
+
+    private void OnGUI()
+    {
+        Vector3 point = new Vector3();
+        Event currentEvent = Event.current;
+        Vector2 mousePos = new Vector2();
+
+        mousePos.x = currentEvent.mousePosition.x;
+        mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+
+        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+
+        GUILayout.BeginArea(new Rect(20, 150, 250, 120));
+        GUILayout.Label("Mouse position: " + mousePos);
+        GUILayout.Label("World position: " + point.ToString("F3"));
+        GUILayout.Label("Player position: " + this.transform.position);
+        GUILayout.Label("Current bullet index: " + currentBulletIndex);
+        GUILayout.EndArea();
     }
     #endregion
 }
